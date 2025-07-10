d37dbbe84cddd5777d35314b0b56f4e3d72d9d3a
Tian Jacobs <tianjosh25@gmail.com>
2025-07-10 10:35:33 +0200

    Fixing

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  optimizeDeps: {
+    include: ['react-icons/fa', 'react-icons/si', 'react-icons/tb', 'react-icons/gr']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-icons': ['react-icons/fa', 'react-icons/si', 'react-icons/tb', 'react-icons/gr']
+        }
+      }
+    }
+  }
+})
